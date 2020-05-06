e631c7b8b67702db2a79b9b80dec2b41d24126ac
Julio Oscar <juliooscar.g@nauta.cu>
2020-05-06 00:39:07 -0400

    Updates

---
+++ precache-manifest.93a01e11d83c03d93796cb8a66783850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "c5f8c2e7cca1f587289b",
+    "url": "/movie-app/static/js/main.c5f8c2e7.chunk.js"
+  },
+  {
+    "revision": "58566f42dce689efac19",
+    "url": "/movie-app/static/js/1.58566f42.chunk.js"
+  },
+  {
+    "revision": "c5f8c2e7cca1f587289b",
+    "url": "/movie-app/static/css/main.f56d31f2.chunk.css"
+  },
+  {
+    "revision": "95c3fe3d05871c9069a5e0f5051ac122",
+    "url": "/movie-app/index.html"
+  }
+];
