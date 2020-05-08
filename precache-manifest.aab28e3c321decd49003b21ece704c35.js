82c84fe81fdceaab412b5813255c6bc388cce67f
Julio Oscar <juliooscar.g@nauta.cu>
2020-05-08 19:37:49 -0400

    Updates

---
+++ precache-manifest.aab28e3c321decd49003b21ece704c35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "1f8c162ad3e3bfb3242a",
+    "url": "/movie-app/static/js/main.1f8c162a.chunk.js"
+  },
+  {
+    "revision": "ab7a0b3fae9a2c787431",
+    "url": "/movie-app/static/js/1.ab7a0b3f.chunk.js"
+  },
+  {
+    "revision": "1f8c162ad3e3bfb3242a",
+    "url": "/movie-app/static/css/main.14b7d9ea.chunk.css"
+  },
+  {
+    "revision": "8e50863f24371e80ae800eec4f765871",
+    "url": "/movie-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-app/precache-manifest.57426f12f92d623cf42812687841ef6a.js"
+  "/movie-app/precache-manifest.aab28e3c321decd49003b21ece704c35.js"
 );
 
 workbox.clientsClaim();
